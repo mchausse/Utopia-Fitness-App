418ed9c4177c44b6d3c30e8d32103c0ef900d1e9
mchausse <maxime.chausse@hotmail.ca>
2021-04-08 22:28:57 -0400

    Closing branch

--- web-app/controleur/DefaultAction.class.php
+++ web-app/controleur/DefaultAction.class.php
@@ -2,10 +2,13 @@
 require_once('./controleur/Action.interface.php');
 class DefaultAction implements Action {
 	public function execute(){
-		if (!ISSET($_SESSION)) session_start();
-		if (!ISSET($_SESSION["connected"]))
+		if (!ISSET($_SESSION)) {
+			session_start();
+		}
+		if (!ISSET($_SESSION["connected"])){
 			return "login";
-		return "default";
+		}
+		return "login";
 	}
 }
 ?>
